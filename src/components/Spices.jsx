03a61f179ac94024600287725d05385ca24c174b
Aman Ruhela <amank004433@gmail.com>
2026-09-21 12:15:01 +0530

    fix burger

--- src/components/Spices.jsx
+++ src/components/Spices.jsx
@@ -18,7 +18,7 @@ const Spices = () => {
             <h1 className="flex justify-center items-center text-center mt-5 font-extrabold text-[#ed2b1c] text-5xl font-['Indie_Flower',cursive] [-webkit-text-stroke:1px_white]">FOOD THAT</h1>
             <h2 className="flex justify-center items-center text-center mt-5 font-extrabold text-[#ed2b1c] text-5xl font-['Indie_Flower',cursive] [-webkit-text-stroke:1px_white]">FEELS GOOD</h2>
         </div>
-        <div ref={ref} className="flex flex-col justify-center items-center ml-20 overflow-hidden  md:flex md:flex-row md:justify-between  mt-25 mr-20 mb-25">
+        <div ref={ref} className="flex flex-col justify-center items-center ml-20   md:flex md:flex-row md:justify-between  mt-25 mr-20 mb-25 ">
             <motion.div 
             animate={
                 isInView
@@ -39,7 +39,7 @@ const Spices = () => {
             transition={{
                 duration:2
             }}
-            className="image w-[60%] md:w-[40%] flex overflow:hidden "
+            className="image w-full md:w-[40%] flex overflow:hidden "
             ><motion.img 
             animate={{
             y:[0,-10,0]
